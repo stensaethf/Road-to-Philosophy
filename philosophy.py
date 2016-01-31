113cc483bc56e437ba12d10520a5fe45f11fdcb3
Frederik Stensaeth <stensaethf@carleton.edu>
2016-01-30 20:52:34 -0600

    error handling and part of philosophy()

--- philosophy.py
+++ philosophy.py
@@ -5,16 +5,27 @@ Frederik Roenn Stensaeth
 Python program to play the game 'all Wikipedia articles lead to philosophy'.
 '''
 
-def philosophy(page_title):
+import sys
+
+def philosophy(page):
 	# load given page.
-	# print title
-	# while page != philosophy:
+	seen = {}
+	seen[page] = True
+	print page
+	while page != 'Philosophy':
 	# 	load that Wikipedia page.
 	# 	grab first link on that page and set page equal to that.
-	# 	print title
+		print page
+		if page in seen:
+			print 'Cycle detected! We have seen \'' + page + '\' before...'
+			sys.exit()
 
 def main():
-	philosophy(sys.args[1])
+	if len(sys.argv) != 2:
+		print 'Sorry, looks like you provided an incorrect number of arguments...'
+		print 'Usage: $ python philosophy.py <starting title>'
+		sys.exit()
+	philosophy(sys.argv[1])
 
 
 if __name__ == '__main__':
