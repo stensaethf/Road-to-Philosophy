2423ba79d089379353497a6e3e8a69e0a519686c
Frederik Stensaeth <stensaethf@carleton.edu>
2016-01-30 20:39:28 -0600

    outlined algorithm

--- philosophy.py
+++ philosophy.py
@@ -4,3 +4,18 @@ Frederik Roenn Stensaeth
 
 Python program to play the game 'all Wikipedia articles lead to philosophy'.
 '''
+
+def philosophy(page_title):
+	# load given page.
+	# print title
+	# while page != philosophy:
+	# 	load that Wikipedia page.
+	# 	grab first link on that page and set page equal to that.
+	# 	print title
+
+def main():
+	philosophy(sys.args[1])
+
+
+if __name__ == '__main__':
+	main()
